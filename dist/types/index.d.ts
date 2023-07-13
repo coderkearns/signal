719d0c9a99c72064240dfc9902206e141bf9ea5d
coderkearns <coder.kearns@gmail.com>
2023-07-12 19:59:54 -0500

    refactor: refactor signal design and internal implementation

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,14 +1,15 @@
+export type Subscriber = () => void;
 interface Signal<T> {
-    get(): T;
-    /** Getter */
-    readonly $: T;
+    _value: T;
+    _subscribers: Array<Subscriber>;
+    (): T;
     set(newValue: T): void;
-    subscribe(callback: () => void): () => void;
+    subscribe(callback: Subscriber): () => void;
     destroy(): void;
     valueOf(): T;
     toString(): string;
 }
-export declare function use<T>(value: T): Signal<T>;
-export declare function s<T>(signal: Signal<T>): T;
-export declare function effect<T>(fn: () => T): Signal<T>;
+export declare function use<T>(initialValue: T): Signal<T>;
+export declare function effect<T>(callback: Subscriber): () => void;
+export declare function derived<T>(deriveFn: () => T): Signal<T>;
 export {};
--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,74 @@
+export type Subscriber = () => void
 type SubscribeFn = (callback: () => void) => () => void
 
-let _signals: SubscribeFn[] | null = null
+let _root: Signal<unknown>[] | null = null
 
 interface Signal<T> {
-    get(): T
-    /** Getter */
-    readonly $: T
+    _value: T
+    _subscribers: Array<Subscriber>
+    
+    (): T
     set(newValue: T): void
-    subscribe(callback: () => void): () => void
+    subscribe(callback: Subscriber): () => void
     destroy(): void
     valueOf(): T
     toString(): string
 }
 
-export function use<T>(value: T): Signal<T> {
-    const subscribers: (() => void)[] = []
-
-    const signal = {
-        get() {
-            if (_signals !== null && !(_signals.includes(signal.subscribe))) {
-                _signals.push(signal.subscribe)
-            }
-            return value
-        },
-        get $() {
-            return signal.get()
-        },
-        set(newValue: T) {
-            value = newValue
-            subscribers.forEach(callback => callback())
-        },
-        subscribe(callback: () => void) {
-            subscribers.push(callback)
-            return () => {
-                subscribers.splice(subscribers.indexOf(callback), 1)
-            }
-        },
-        destroy() { },
-        valueOf() {
-            return signal.get()
-        },
-        toString() {
-            return `${signal.get()}`
+const signalProto: Signal<unknown> = {
+    set(this: Signal<unknown>, newValue: unknown) {
+        this._value = newValue
+        this._subscribers.forEach(callback => callback())
+    },
+    subscribe(this: Signal<unknown>, callback: Subscriber) {
+        this._subscribers.push(callback)
+        return () => {
+            this._subscribers.splice(this._subscribers.indexOf(callback), 1)
         }
+    },
+    destroy(this: Signal<unknown>) { },
+    valueOf(this: Signal<unknown>) {
+        return this()
+    },
+    toString(this: Signal<unknown>) {
+        return `${this()}`
     }
+} as Signal<unknown>
 
-    return signal
+export function use<T>(initialValue: T): Signal<T> {
+    const signal = function(this: Signal<T>) {
+        if (_root !== null) _root.push(this)
+        return this._value
+    }
+    
+    Object.assign(signal, signalProto, {
+        _value:  initialValue,
+        _subscribers: []  
+    })
+    
+    return signal as Signal<T>
 }
 
-export function effect<T>(fn: () => T): Signal<T> {
-    _signals = []
-    const signal = use(fn())
-    const recompute = () => signal.set(fn())
-    const unsubscribers = _signals.map(s => s(recompute))
-    signal.destroy = () => {
-        unsubscribers.forEach(u => u())
-    }
-    _signals = null
+function withRoot<T>(fn: () => T): { signals: Array<Signal<unknown>>, ret: T } {
+    const oldRoot = _root
+    _root = []
+    const ret = fn()
+    const newRoot = _root
+    _root = oldRoot
+    return { signals: newRoot, ret }
+}
+
+export function effect<T>(callback: Subscriber): () => void {
+    const { signals } = withRoot(callback)
+    const unsubscribers = signals.map(signal => signal.subscribe(callback))
+    return () => unsubscribers.forEach(unsubscribe => unsubscribe())
+}
+
+export function derived<T>(deriveFn: () => T): Signal<T> {
+    const { signals, ret } = withRoot(deriveFn)
+    const signal = use(ret)
+    const callback = () => signal.set(deriveFn())
+    const unsubscribers = signals.map(signal => signal.subscribe(callback))
+    signal.destroy = () => unsubscribers.forEach(unsubscribe => unsubscribe())
     return signal
 }
